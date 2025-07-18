a851744166eb238b4cfc32adfc3bb8c4f956f676
molnarszilard <molnarszilard10@gmail.com>
2025-07-18 14:21:16 +0300

    p3 and xavier agx

---
+++ deployment/src/record_bag_p3.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Create a new tmux session
+session_name="record_bag_$(date +%s)"
+tmux new-session -d -s $session_name
+
+# Split the window into three panes
+tmux selectp -t 0    # select the first (0) pane
+tmux splitw -v -p 50 # split it into two halves
+tmux selectp -t 0    # go back to the first pane
+tmux splitw -h -p 50 # split it into two halves
+
+# Run the roslaunch command in the first pane
+tmux select-pane -t 0
+tmux send-keys "source /home/rocon/catkin_ws/devel/setup.bash" Enter
+tmux send-keys "roslaunch vint_p3.launch" Enter
+
+# Run the teleop.py script in the second pane
+tmux select-pane -t 1
+tmux send-keys "rosrun teleop_twist_keyboard teleop_twist_keyboard.py /cmd_vel:=/RosAria/cmd_vel" Enter
+
+# Change the directory to ../topomaps/bags and run the rosbag record command in the third pane
+tmux select-pane -t 2
+tmux send-keys "cd ../topomaps/bags" Enter
+tmux send-keys "rosbag record /usb_cam/image_raw -o $1" # change topic if necessary
+
+# Attach to the tmux session
+tmux -2 attach-session -t $session_name
